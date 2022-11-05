67df1e90dcc63c9ff99802e7e2422ce2041f1095
aThinkingNeal <31988374+aThinkingNeal@users.noreply.github.com>
2022-11-05 12:06:06 +0800

    Add comment for the function

--- SenticNet-polarity-detection-single-datum.py
+++ SenticNet-polarity-detection-single-datum.py
@@ -1,6 +1,14 @@
 import requests, xlrd, xlwt
 
 def getPolaritySenticNet(datum):
+    '''This function performs polarity detection using SenticNet API
+    Input:
+        * datum: a sentence in string format
+    
+    Output:
+        * label: polarity detection result in string format ["NEGATIVE", "NEUTRAL", "POSITIVE"]
+    
+    '''
     LANG = 'en'
     APIKEY = 'jFz4p0loUTjnz'
     APIURL = 'https://sentic.net/api/' + LANG + '/' + APIKEY + '.py?text='
